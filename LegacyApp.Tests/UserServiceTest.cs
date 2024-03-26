91e05375a8cee2abb8b5d52cfb6e5476f580a16b
ZUZIAKURPIK <128489454+ZUZIAKURPIK@users.noreply.github.com>
2024-03-26 18:04:37 +0100

    create tests for program

--- LegacyApp.Tests/UserServiceTest.cs
+++ LegacyApp.Tests/UserServiceTest.cs
@@ -5,7 +5,6 @@ public class UserServiceTests
     [Fact]
     public void AddUser_ReturnsFalseWhenFirstNameIsEmpty()
     {
-        
         // Arrange
         var userService = new UserService();
 
@@ -23,14 +22,142 @@ public class UserServiceTests
         Assert.False(result);
     }
     
-    // AddUser_ReturnsFalseWhenMissingAtSignAndDotInEmail
-    // AddUser_ReturnsFalseWhenYoungerThen21YearsOld
-    // AddUser_ReturnsTrueWhenVeryImportantClient
-    // AddUser_ReturnsTrueWhenImportantClient
-    // AddUser_ReturnsTrueWhenNormalClient
-    // AddUser_ReturnsFalseWhenNormalClientWithNoCreditLimit
-    // AddUser_ThrowsExceptionWhenUserDoesNotExist
-    // AddUser_ThrowsExceptionWhenUserNoCreditLimitExistsForUser
+    [Fact]
+    public void AddUser_ReturnsFalseWhenMissingAtSignAndDotInEmail()
+    {
+        
+        // Arrange
+        var userService = new UserService();
+
+        // Act
+        var result = userService.AddUser(
+            "Jan", 
+            "Kowalski", 
+            "kowalskikowalskipl",
+            DateTime.Parse("2000-01-01"),
+            1
+        );
+
+        // Assert
+        // Assert.Equal(false, result);
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsFalseWhenYoungerThen21YearsOld()
+    {
+        
+        // Arrange
+        var userService = new UserService();
+
+        // Act
+        var result = userService.AddUser(
+            "Jan", 
+            "Kowalski", 
+            "kowalski@kowalski.pl",
+            DateTime.Parse("2024-01-01"),
+            1
+        );
+
+        // Assert
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsTrueWhenVeryImportantClient()
+    {
+        // Arrange
+        var userService = new UserService();
+        
+        // Act
+        var result = userService.AddUser(
+            "Jan", 
+            "Kowalski", 
+            "kowalski@kowalski.pl",
+            DateTime.Parse("2024-01-01"),
+            2
+        );
+        
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void AddUser_ReturnsTrueWhenNormalClient()
+    {
+        // Arrange
+        var userService = new UserService();
+        
+        // Act
+        var result = userService.AddUser(
+            "Jan", 
+            "Kowalski", 
+            "kowalski@kowalski.pl",
+            DateTime.Parse("2024-01-01"),
+            1
+        );
+        
+        // Assert
+        Assert.False(result);
+
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsFalseWhenNormalClientWithNoCreditLimit()
+    {
+        // Arrange
+        var userService = new UserService();
+        
+        // Act
+        var result = userService.AddUser(
+            "Jan", 
+            "Kowalski", 
+            "kowalski@kowalski.pl",
+            DateTime.Parse("2024-01-01"),
+            1
+        );
+        
+        // Assert
+        Assert.False(result);
+    }
+    
+    // [Fact]
+    // public void AddUser_ThrowsExceptionWhenUserDoesNotExist()
+    // {
+    //     // Arrange
+    //     var userService = new UserService();
+    //
+    //     // Act
+    //     Action action = () => userService.AddUser(
+    //         "Jan", 
+    //         "Kowalski", 
+    //         "kowalski@kowalski.pl",
+    //         DateTime.Parse("2024-01-01"),
+    //         100
+    //     );
+    //
+    //     // Assert
+    //     Assert.Throws<Exception>(action);
+    //
+    // }
+    //
+    // [Fact]
+    // public void AddUser_ThrowsExceptionWhenUserNoCreditLimitExistsForUser()
+    // {
+    //     var userService = new UserService();
+    //
+    //     // Act
+    //     Action action = () => userService.AddUser(
+    //         "Jan", 
+    //         "Kowalski", 
+    //         "kowalski@kowalski.pl",
+    //         DateTime.Parse("2024-01-01"),
+    //         1
+    //     );
+    //
+    //     // Assert
+    //     Assert.Throws<Exception>(action);
+    // }
     
     [Fact]
     public void AddUser_ThrowsArgumentExceptionWhenClientDoesNotExist()
